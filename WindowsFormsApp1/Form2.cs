a284875215ecd7c840187361323c6f5281f00068
Veronika Zarubo <nikazarubo1@gmail.com>
2024-12-06 22:45:52 +0100

    Add project files.

---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string login;
+            string pass;
+            login = textBox1.Text;
+            pass = textBox2.Text;
+
+            if (string.IsNullOrEmpty(login) && string.IsNullOrEmpty(pass))
+                MessageBox.Show("Proszę podać login i hasło");
+            else if (string.IsNullOrEmpty(login))
+                MessageBox.Show("Proszę podać login");
+            else if (string.IsNullOrEmpty(pass))
+                MessageBox.Show("Proszę podać hasło");
+            else if (login == pass)
+            {
+                MessageBox.Show("Udane logowanie");
+                Menu newLevel = new Menu();
+                this.Hide();
+                newLevel.Show();
+            }
+            else MessageBox.Show("Zły login lub hasło");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 newLevel = new Form1();
+            this.Hide();
+            newLevel.Show();
+        }
+    }
+}
---
+++ WindowsFormsApp1/Menu.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.nASILEKARZEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fUFKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nASILEKARZEToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // nASILEKARZEToolStripMenuItem
+            // 
+            this.nASILEKARZEToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fUFKToolStripMenuItem,
+            this.tToolStripMenuItem});
+            this.nASILEKARZEToolStripMenuItem.Name = "nASILEKARZEToolStripMenuItem";
+            this.nASILEKARZEToolStripMenuItem.Size = new System.Drawing.Size(117, 24);
+            this.nASILEKARZEToolStripMenuItem.Text = "nASI LEKARZE";
+            // 
+            // fUFKToolStripMenuItem
+            // 
+            this.fUFKToolStripMenuItem.Name = "fUFKToolStripMenuItem";
+            this.fUFKToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.fUFKToolStripMenuItem.Text = "FUFK";
+            // 
+            // tToolStripMenuItem
+            // 
+            this.tToolStripMenuItem.Name = "tToolStripMenuItem";
+            this.tToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.tToolStripMenuItem.Text = "6T";
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.Load += new System.EventHandler(this.Menu_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nASILEKARZEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fUFKToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tToolStripMenuItem;
+    }
+}
