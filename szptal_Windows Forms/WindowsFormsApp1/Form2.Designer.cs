94ae188056669798d80de80cb9c65780b55b9664
Veronika Zarubo <nikazarubo1@gmail.com>
2024-12-14 17:27:14 +0100

    Commit with a solution

--- szptal_Windows Forms/WindowsFormsApp1/Form2.Designer.cs
+++ szptal_Windows Forms/WindowsFormsApp1/Form2.Designer.cs
@@ -125,6 +125,6 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label1; 
     }
 }
